27fca3e2b66c2fafa1eaa6b50f465dbb4bf37054
austinGENEreeve <austinGENEreeve@gmail.com>
2020-08-06 12:15:40 -0600

    Camera Follow script

---
+++ Assets/_scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+  public Transform player;
+  public Vector3 offset;
+  public float followSpeed;
+
+  void Update ()
+  {
+      Vector3 target = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z);
+      Vector3 currentPos = Vector3.Lerp(transform.position, target, followSpeed);
+      this.transform.position = currentPos;
+  }
+}
