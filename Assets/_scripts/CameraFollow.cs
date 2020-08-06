2b3e9c1631f75b851d5faf05562c0accc3839391
austinGENEreeve <austinGENEreeve@gmail.com>
2020-08-06 12:17:15 -0600

    camera follow

--- Assets/_scripts/CameraFollow.cs
+++ Assets/_scripts/CameraFollow.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +8,7 @@ public class CameraFollow : MonoBehaviour
   public Vector3 offset;
   public float followSpeed;
 
+ 
   void Update ()
   {
       Vector3 target = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z);
